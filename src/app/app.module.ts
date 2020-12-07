aa54d8df746a7f94619525417410b65fb014f093
枋柚梓 <me@szyink.com>
2020-12-07 16:12:17 +0800

    基础框架修改

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {IconsProviderModule} from './icons-provider.module';
-import {NzLayoutModule} from 'ng-zorro-antd/layout';
-import {NzMenuModule} from 'ng-zorro-antd/menu';
-import {FormsModule} from '@angular/forms';
+import {IconsProviderModule} from './config/icons-provider.module';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NZ_I18N, zh_CN} from 'ng-zorro-antd/i18n';
 import {registerLocaleData} from '@angular/common';
+import {NgZorroAntdModule} from './config/ng-zorro-antd.module';
 import zh from '@angular/common/locales/zh';
 
 registerLocaleData(zh);
@@ -21,11 +17,8 @@ registerLocaleData(zh);
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     IconsProviderModule,
-    NzLayoutModule,
-    NzMenuModule,
-    FormsModule,
+    NgZorroAntdModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
