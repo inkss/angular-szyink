dbd95fd43f07d2114a2f64588312586e33e42b6b
枋柚梓 <me@szyink.com>
2020-12-29 17:37:41 +0800

    更新

--- src/app/routes/pages/register/register.component.ts
+++ src/app/routes/pages/register/register.component.ts
@@ -8,7 +8,8 @@ import {Router} from '@angular/router';
   styleUrls: ['./register.component.less']
 })
 export class RegisterComponent implements OnInit {
-  passwordVisible = false;
+  pwdVisible = false;
+  confirmPwdVisible = false;
   validateForm: FormGroup;
   router: Router;
 
@@ -22,6 +23,7 @@ export class RegisterComponent implements OnInit {
     this.validateForm = this.fb.group({
       userName: [null, [Validators.required]],
       userPwd: [null, [Validators.required, this.validList.validateWeakPwd]],
+      confirmPwd: [null, [Validators.required, this.validList.validateCheckPwd]]
     });
   }
 
@@ -35,6 +37,17 @@ export class RegisterComponent implements OnInit {
       }
       return {};
     },
+    validateCheckPwd: (control: FormControl): { [s: string]: boolean } => {
+      if (!control.value) {
+        return {required: true};
+      } else if (control.value !== this.validateForm.controls.userPwd.value) {
+        return {confirm: true, error: true};
+      }
+      return {};
+    },
+    confirmPassword: () => {
+      setTimeout(() => this.validateForm.controls.confirmPwd.updateValueAndValidity());
+    }
   };
 
   submitForm(): void {
